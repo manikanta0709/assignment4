e5ce187314886b5e09879f0cbbbde01254d2f1db
manikanta0709 <manibunny0718@gmail.com>
2022-06-15 08:15:42 +0530

    added: some program

---
+++ random/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+
+Random random = new Random();
+const int priceperhr= 20, fullday=8,ispresent = 1;
+int res = random.Next(0, 2);
+if(res==1)
+{
+    Console.WriteLine("absent");
+    Console.WriteLine("wage:" + (priceperhr * fullday));
+}
+else
+{
+    Console.WriteLine("present");
+}*/
+for(int i=1;i<=10;i++)
+{
+
+    Console.WriteLine(i);
+}
